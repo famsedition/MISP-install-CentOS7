ade2b9a5e104b4483a691bc81ddceb2f74f05979
fbodian <famarabodian@protonmail.com>
2019-09-27 09:02:58 +0000

    upload

---
+++ misp-preparations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#######################################################################
+#                    MISP CENTOS7 INSTALL SCRIPT                      #
+#                                                                     #
+# Revised from:                                                       #
+# https://misp.github.io/MISP/INSTALL.rhel7/  						  #
+#                                                                     #
+# > Must be run as root                                               #
+# > run this file first 										  	  #
+#######################################################################
+
+hostnamectl set-hostname misp.local
+yum update -y
+yum install epel-release -y	
+yum install centos-release-scl -y
+yum install deltarpm -y
+useradd -rU misp
+reboot 		
